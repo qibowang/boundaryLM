359d8993ce413af341c91211e1b0733eb480b58e
qibowang <qibowang@hotmail.com>
2018-02-05 21:15:26 +0800

    add the numerator.sh

--- lm_few_separator_0204/src/cn/edu/blcu/nlp/middleSegRawcountDenominator/middleSegRawcountDenominator.sh
+++ lm_few_separator_0204/src/cn/edu/blcu/nlp/middleSegRawcountDenominator/middleSegRawcountDenominator.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 startOrder=1
-endOrder=7
+endOrder=6
 tasks=1
 isLzo=1
-input=
-rawCountPath=
+#input=/user/wangqibo/data/coupus/seg0/chuli_blog/049_.txt.txt_out
+input=/user/wangqibo/data/coupus/seg0/lishijisuan_5/2013080113_02.txt.txt_out
+rawCountPath=/user/wangqibo/20180205_mid_seg/rawcount_denominator
 
-hadoop jar -input $input -rawcount $rawCountPath -startOrder $startOrder -endOrder $endOrder -tasks $tasks -isLzo $isLzo
+hadoop jar middleSegRawcountDenominator.jar -input $input -rawcount $rawCountPath -startOrder $startOrder -endOrder $endOrder -tasks $tasks -isLzo $isLzo 
